4bcd02374f746b6ffe72b0a8aa01bb4243fa53b3
Dede Fuji Abdul <defuj.official@gmail.com>
2024-06-05 10:29:29 +0700

    Refactor CSS styles and add new component IconChevronRight

---
+++ src/components/icons/IconChevronRight.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="43"
+    height="42"
+    viewBox="0 0 43 42"
+    fill="currentColor"
+  >
+    <g clip-path="url(#clip0_5110_325)">
+      <path
+        d="M16.9242 11.7425C16.2417 12.425 16.2417 13.5275 16.9242 14.21L23.7142 21L16.9242 27.79C16.2417 28.4725 16.2417 29.575 16.9242 30.2575C17.6067 30.94 18.7092 30.94 19.3917 30.2575L27.4242 22.225C28.1067 21.5425 28.1067 20.44 27.4242 19.7575L19.3917 11.725C18.7267 11.06 17.6067 11.06 16.9242 11.7425Z"
+        fill="#68769F"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_5110_325">
+        <rect width="42" height="42" fill="white" transform="translate(0.666748)" />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Moon, Phone } from '@/components/icons'
+import { ChevronRight, Moon, Phone } from '@/components/icons'
 import { RouterLink } from 'vue-router'
 </script>
 
@@ -74,6 +74,54 @@ import { RouterLink } from 'vue-router'
         <p class="mt-3 text-base font-medium leading-8 text-center text-dark-grey-600">
           Tellus rutrum tellus pellentesque eu tincidunt tortor condimentum.
         </p>
+
+        <div class="w-full mt-[60px] flex flex-row justify-between items-center space-x-[40px]">
+          <div class="flex flex-row justify-between flex-1 space-x-5">
+            <div
+              class="flex items-center justify-center w-12 h-12 text-base font-bold leading-8 text-center text-white rounded-full bg-purple-blue-500"
+            >
+              1
+            </div>
+            <div class="flex flex-col flex-1">
+              <h1 class="text-base font-bold leading-8 text-dark-grey-900">Create your Account</h1>
+              <p class="text-base font-medium leading-8 text-dark-grey-600">
+                Condimentum vit pellemsque habitant morbi at molestie.
+              </p>
+            </div>
+          </div>
+
+          <ChevronRight class="w-10 h-10 text-dark-grey-600" />
+
+          <div class="flex flex-row justify-between flex-1 space-x-5">
+            <div
+              class="flex items-center justify-center w-12 h-12 text-base font-bold leading-8 text-center bg-white border-2 rounded-full text-purple-blue-500 border-purple-blue-500"
+            >
+              2
+            </div>
+            <div class="flex flex-col flex-1">
+              <h1 class="text-base font-bold leading-8 text-dark-grey-900">Create your Account</h1>
+              <p class="text-base font-medium leading-8 text-dark-grey-600">
+                Condimentum vit pellemsque habitant morbi at molestie.
+              </p>
+            </div>
+          </div>
+
+          <ChevronRight class="w-10 h-10 text-dark-grey-600" />
+
+          <div class="flex flex-row justify-between flex-1 space-x-5">
+            <div
+              class="flex items-center justify-center w-12 h-12 text-base font-bold leading-8 text-center bg-white border-2 rounded-full text-purple-blue-500 border-purple-blue-500"
+            >
+              3
+            </div>
+            <div class="flex flex-col flex-1">
+              <h1 class="text-base font-bold leading-8 text-dark-grey-900">Create your Account</h1>
+              <p class="text-base font-medium leading-8 text-dark-grey-600">
+                Condimentum vit pellemsque habitant morbi at molestie.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </main>
